ab8da2d82ba63e12dd32f0c4dbc8c61717fedea7
Abdullah Osama <abdullahelagami55@gmail.com>
2022-02-07 18:18:43 +0200

    add search reading table by start and end date and time

--- src/main/java/com/root/meter/repo/ReadingRepo.java
+++ src/main/java/com/root/meter/repo/ReadingRepo.java
@@ -4,10 +4,13 @@ import com.root.meter.model.Reading;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ReadingRepo extends CrudRepository<Reading, Long> {
     Optional<List<Reading>> findAllByMeterIdId(Long meterId);
+
+    Optional<List<Reading>> findAllByTimeStampBetween(LocalDateTime start, LocalDateTime end);
 }
