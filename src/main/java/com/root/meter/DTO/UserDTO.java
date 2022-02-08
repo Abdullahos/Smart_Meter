0e435eeddda4248850c9b4a520f154ba4b386008
Abdullah Osama <abdullahelagami55@gmail.com>
2022-02-08 13:36:59 +0200

    create user dto

---
+++ src/main/java/com/root/meter/DTO/UserDTO.java
@@ -0,0 +1,86 @@
+package com.root.meter.DTO;
+
+public class UserDTO {
+
+    private Long meterId;
+    private String name;
+    private String email;
+    private String password;
+    private String state;
+    private String phone;
+    private String address;
+    private String creditCardNo;
+    private String  cvv; //card verification value for payment online or over the phone.
+
+    public Long getMeterId() {
+        return meterId;
+    }
+
+    public void setMeterId(Long meterId) {
+        this.meterId = meterId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCreditCardNo() {
+        return creditCardNo;
+    }
+
+    public void setCreditCardNo(String creditCardNo) {
+        this.creditCardNo = creditCardNo;
+    }
+
+    public String getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(String cvv) {
+        this.cvv = cvv;
+    }
+}
