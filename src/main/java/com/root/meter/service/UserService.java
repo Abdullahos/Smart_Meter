45539dfff9c2d1b1137f88ca16555c3064999824
Abdullah Osama <abdullahelagami55@gmail.com>
2022-02-09 22:18:39 +0200

    fix the flushing error by placing Transactional

--- src/main/java/com/root/meter/service/UserService.java
+++ src/main/java/com/root/meter/service/UserService.java
@@ -4,26 +4,43 @@ import com.root.meter.DTO.UserDTO;
 import com.root.meter.Exception.ObjectNotFoundException;
 import com.root.meter.model.Meter;
 import com.root.meter.model.User;
-import com.root.meter.repo.MeterRepo;
 import com.root.meter.repo.UserRepo;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
-
+@Transactional
 @Service
 public class UserService {
     @Autowired
     private UserRepo userRepo;
     @Autowired
-    private MeterRepo meterRepo;
+    private MeterService meterService;
 
+    /**
+     * create user and meter and assign them to each other
+     * @param userDTO
+     * @return the saved user
+     */
     public User save(UserDTO userDTO){
-        //TODO: add filter
-        return userRepo.save(dtoToUser(userDTO));
+        User user = dtoToUser(userDTO);
+        //assign user to meter
+        Meter meter = new Meter(user);
+        //assign meter to user
+        user.setMeter(meter);
+        //save user
+        meterService.save(meter);
+        //save and return user
+        return userRepo.save(user);
     }
 
+    /**
+     * find user by meter id
+     * @param meterId
+     * @return the user of that meter id or throw Object Not Found
+     */
     public User findUserByMeterId(Long meterId){
         Optional<User> optionalUser = userRepo.findByMeterId(meterId);
         if(optionalUser.isPresent()){
@@ -33,6 +50,7 @@ public class UserService {
             throw new ObjectNotFoundException("no user linked to this meter with id: "+meterId);
         }
     }
+    //convert user to userDto
     public UserDTO userToDTO(User user){
         UserDTO dto = new UserDTO();
         BeanUtils.copyProperties(user, dto);
@@ -41,16 +59,9 @@ public class UserService {
         return dto;
     }
     public User dtoToUser(UserDTO dto){
+        //TODO: dto may be null (check)
         User user = new User();
         BeanUtils.copyProperties(dto, user);
-        //TODO: dto may be null (check)
-        Optional<Meter> optionalMeterById = meterRepo.findById(dto.getMeterId());
-        if(optionalMeterById.isPresent()){
-            user.setMeter(optionalMeterById.get());
-            return user;
-        }
-        else {
-            throw new ObjectNotFoundException("no such meter");
-        }
+        return user;
     }
 }
