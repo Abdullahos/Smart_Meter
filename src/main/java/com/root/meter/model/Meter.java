f6fa850e5a0508d46bd75973af2a45675699a98f
Abdullah Osama <abdullahelagami55@gmail.com>
2022-02-09 22:04:19 +0200

    add json ignore to user to avoid infinite revursion

--- src/main/java/com/root/meter/model/Meter.java
+++ src/main/java/com/root/meter/model/Meter.java
@@ -1,12 +1,25 @@
 package com.root.meter.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+import javax.transaction.Transactional;
 
 @Entity
+@Transactional
 public class Meter {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @OneToOne
+    @JoinColumn(name = "userId")
+    @JsonIgnore //to avoid infinite recursion meter->user->meter->user->....
+    private User user;
+
+    public Meter(User user) {
+        this.user = user;
+    }
 
     public Long getId() {
         return id;
@@ -15,4 +28,13 @@ public class Meter {
     public void setId(Long id) {
         this.id = id;
     }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
 }
