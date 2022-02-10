606223fefd16d962494cd933f7fba6b84b74f726
Abdullah Osama <abdullahelagami55@gmail.com>
2022-02-10 16:42:46 +0200

    add Transactional annotation

--- src/main/java/com/root/meter/repo/UserRepo.java
+++ src/main/java/com/root/meter/repo/UserRepo.java
@@ -3,8 +3,9 @@ package com.root.meter.repo;
 import com.root.meter.model.User;
 import org.springframework.data.repository.CrudRepository;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
-
+@Transactional
 public interface UserRepo extends CrudRepository<User,Long> {
     Optional<User> findByMeterId(Long meterId);
 }
