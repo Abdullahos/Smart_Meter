8c54e839100cb25808cceaba3235f6d1101f977c
Abdullah Osama <abdullahelagami55@gmail.com>
2022-02-10 16:42:16 +0200

    add Transactional annotation

--- src/main/java/com/root/meter/repo/MeterRepo.java
+++ src/main/java/com/root/meter/repo/MeterRepo.java
@@ -4,6 +4,9 @@ import com.root.meter.model.Meter;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
+@Transactional
 @Repository
 public interface MeterRepo extends CrudRepository<Meter, Long > {
 }
