59d334318e612729912f115ec291e6785557858e
Abdullah Osama <abdullahelagami55@gmail.com>
2022-02-10 16:41:42 +0200

    rename meterId to meter

--- src/main/java/com/root/meter/repo/ReadingRepo.java
+++ src/main/java/com/root/meter/repo/ReadingRepo.java
@@ -1,16 +1,19 @@
 package com.root.meter.repo;
 
-import com.root.meter.model.Reading;
+import com.root.meter.model.DailyReading;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
+import javax.transaction.Transactional;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
-
+@Transactional
 @Repository
-public interface ReadingRepo extends CrudRepository<Reading, Long> {
-    Optional<List<Reading>> findAllByMeterIdId(Long meterId);
+public interface ReadingRepo extends CrudRepository<DailyReading, Long> {
+    Optional<List<DailyReading>> findAllByMeterId(Long meterId);
 
-    Optional<List<Reading>> findAllByTimeStampBetween(LocalDateTime start, LocalDateTime end);
+    Optional<List<DailyReading>> findAllByDateBetween(LocalDate start, LocalDate end);
+
+    Optional<DailyReading> findAllByMeterIdAndDate(Long meterId, LocalDate date);
 }
